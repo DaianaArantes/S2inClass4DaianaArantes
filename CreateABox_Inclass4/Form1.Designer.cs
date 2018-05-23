35660cf2062bbe782371b676402a99b5fa0cdf21
DaianaArantes <32435352+DaianaArantes@users.noreply.github.com>
2018-05-23 10:17:13 -0400

    Create Project
    Create Project

---
+++ CreateABox_Inclass4/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace CreateABox_Inclass4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.labelMsg = new System.Windows.Forms.Label();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxZ2 = new System.Windows.Forms.TextBox();
+            this.textBoxZ1 = new System.Windows.Forms.TextBox();
+            this.textBoxY1 = new System.Windows.Forms.TextBox();
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBoxY2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Blue;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Location = new System.Drawing.Point(169, 506);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(378, 33);
+            this.label10.TabIndex = 35;
+            this.label10.Text = "By Lixin Feng";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelMsg
+            // 
+            this.labelMsg.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.labelMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMsg.Location = new System.Drawing.Point(6, 22);
+            this.labelMsg.Name = "labelMsg";
+            this.labelMsg.Size = new System.Drawing.Size(369, 261);
+            this.labelMsg.TabIndex = 0;
+            this.labelMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Location = new System.Drawing.Point(172, 461);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisplay.TabIndex = 34;
+            this.buttonDisplay.Text = "Display";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            this.buttonDisplay.Click += new System.EventHandler(this.buttonDisplay_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelMsg);
+            this.groupBox1.Location = new System.Drawing.Point(172, 175);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 280);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Processed";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(404, 110);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 20);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Z2";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(404, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 20);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Z1";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(297, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Y1";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(194, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "X1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Blue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 33);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Inclass 4";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxZ2
+            // 
+            this.textBoxZ2.Location = new System.Drawing.Point(448, 110);
+            this.textBoxZ2.Name = "textBoxZ2";
+            this.textBoxZ2.Size = new System.Drawing.Size(46, 20);
+            this.textBoxZ2.TabIndex = 23;
+            // 
+            // textBoxZ1
+            // 
+            this.textBoxZ1.Location = new System.Drawing.Point(448, 71);
+            this.textBoxZ1.Name = "textBoxZ1";
+            this.textBoxZ1.Size = new System.Drawing.Size(46, 20);
+            this.textBoxZ1.TabIndex = 20;
+            // 
+            // textBoxY1
+            // 
+            this.textBoxY1.Location = new System.Drawing.Point(341, 71);
+            this.textBoxY1.Name = "textBoxY1";
+            this.textBoxY1.Size = new System.Drawing.Size(46, 20);
+            this.textBoxY1.TabIndex = 19;
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Location = new System.Drawing.Point(238, 71);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(46, 20);
+            this.textBoxX1.TabIndex = 18;
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label13.Location = new System.Drawing.Point(297, 110);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(38, 20);
+            this.label13.TabIndex = 40;
+            this.label13.Text = "Y2";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxY2
+            // 
+            this.textBoxY2.Location = new System.Drawing.Point(341, 111);
+            this.textBoxY2.Name = "textBoxY2";
+            this.textBoxY2.Size = new System.Drawing.Size(46, 20);
+            this.textBoxY2.TabIndex = 37;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(194, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "X2";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Location = new System.Drawing.Point(238, 112);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(46, 20);
+            this.textBoxX2.TabIndex = 41;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 664);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBoxY2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.buttonDisplay);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxZ2);
+            this.Controls.Add(this.textBoxZ1);
+            this.Controls.Add(this.textBoxY1);
+            this.Controls.Add(this.textBoxX1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labelMsg;
+        private System.Windows.Forms.Button buttonDisplay;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxZ2;
+        private System.Windows.Forms.TextBox textBoxZ1;
+        private System.Windows.Forms.TextBox textBoxY1;
+        private System.Windows.Forms.TextBox textBoxX1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBoxY2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxX2;
+    }
+}
+
---
+++ CreateABox_Inclass4/Form1.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Daiana Arantes
+ * April, 2, 2018
+ * Inclass - 4 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateABox_Inclass4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+
+            public Point (int a, int b, int c)
+            {
+                X = a;
+                Y = b;
+                Z = c;
+            }
+        }
+
+        public class Line
+        {
+            public Point Pt1 {get; set; }
+            public Point Pt2 { get; set; }
+        }
+
+        public class Box
+        {
+            public Line Ln1 { get; set; }
+            public Line Ln2 { get; set; }
+            public Line Ln3 { get; set; }
+        }
+
+        //Method to calculate area of the box
+        public int CalArea(int l, int w)
+        {
+            return l * w;
+        }
+
+        //Method to calculate Volume of the box
+        public int CalVol(int l, int w, int h)
+        {
+            return l * w * h;
+        }
+
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+
+            labelMsg.Text = "";
+            string msg = "";
+            int l = 0, w = 0, h = 0;
+            int s1 = 0, s2 = 0, s3 = 0;
+            int volume = 0;
+
+            Box myBox = new Box();
+            myBox.Ln1 = new Line();
+            myBox.Ln2 = new Line();
+            myBox.Ln3 = new Line();
+
+            myBox.Ln1.Pt1 = new Point(Convert.ToInt16(textBoxX1.Text), Convert.ToInt16(textBoxY1.Text), Convert.ToInt16(textBoxZ1.Text));
+            myBox.Ln1.Pt2 = new Point(Convert.ToInt16(textBoxX1.Text), Convert.ToInt16(textBoxY1.Text), Convert.ToInt16(textBoxZ2.Text));
+
+            myBox.Ln2.Pt1 = new Point(Convert.ToInt16(textBoxX1.Text), Convert.ToInt16(textBoxY1.Text), Convert.ToInt16(textBoxZ1.Text));
+            myBox.Ln2.Pt2 = new Point(Convert.ToInt16(textBoxX1.Text), Convert.ToInt16(textBoxY2.Text), Convert.ToInt16(textBoxZ1.Text));
+
+            myBox.Ln3.Pt1 = new Point(Convert.ToInt16(textBoxX1.Text), Convert.ToInt16(textBoxY1.Text), Convert.ToInt16(textBoxZ1.Text));
+            myBox.Ln3.Pt2 = new Point(Convert.ToInt16(textBoxX2.Text), Convert.ToInt16(textBoxY1.Text), Convert.ToInt16(textBoxZ1.Text));
+
+            msg = "The box contains the following lines:\n";
+            
+            msg += "Line 1: Point 1 (" + myBox.Ln1.Pt1.X + "," + myBox.Ln1.Pt1.Y + "," + myBox.Ln1.Pt1.Z + ")" +
+           " ," + "Point 2 (" + myBox.Ln1.Pt2.X + ", " + myBox.Ln1.Pt2.Y + ", " + myBox.Ln1.Pt2.Z + ")\n";
+            msg += "Line 2: Point 1 (" + myBox.Ln2.Pt1.X + "," + myBox.Ln2.Pt1.Y + "," + myBox.Ln2.Pt1.Z + ")" +
+                " ," + "Point 2 (" + myBox.Ln2.Pt2.X + ", " + myBox.Ln2.Pt2.Y + ", " + myBox.Ln2.Pt2.Z + ")\n";
+            msg += "Line 3: Point 1 (" + myBox.Ln3.Pt1.X + "," + myBox.Ln3.Pt1.Y + "," + myBox.Ln3.Pt1.Z + ")" +
+                " ," + "Point 2 (" + myBox.Ln3.Pt2.X + ", " + myBox.Ln3.Pt2.Y + ", " + myBox.Ln3.Pt2.Z + ")\n";
+
+
+            l =Math.Abs(Convert.ToInt16(textBoxX2.Text) - Convert.ToInt16(textBoxX1.Text));
+            w = Math.Abs(Convert.ToInt16(textBoxY2.Text) - Convert.ToInt16(textBoxY1.Text));
+            h= Math.Abs(Convert.ToInt16(textBoxZ2.Text)-Convert.ToInt16(textBoxZ1.Text));
+
+            s1 = CalArea(l, w);
+            s2 = CalArea(l, h);
+            s3 = CalArea(h, w);
+
+            volume=CalVol(l, w, h);
+
+            msg += "Area S1: " + s1.ToString()+"\n";
+            msg += "Area S2: " + s2.ToString() + "\n";
+            msg += "Area S3: " + s3.ToString() + "\n\n";
+            msg += "Total surface area: " + ((s1 + s2 + s3) * 2).ToString() + "\n\n";
+
+            msg += "Volume: " + volume.ToString() + "\n";
+
+            labelMsg.Text = msg;
+        }
+    }
+}
